bfb123bc366da54205dda932b55f0f2ca0a828cf
DaCHRIS <dachrls0@gmail.com>
2017-10-21 12:17:18 +0200

    Update poolstart.sh

--- poolstart.sh
+++ poolstart.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 #ellamain:
-screen -dmS ellaparity parity --no-ui --author="0x0000000000" --unlock="0x0000000000" --password="~/community-wallet/.walletpass" --max-peers=500 --extra-data="ella.miner" --identity="ella.miner" --cache=128 --chain "~/community-wallet/ellaism.json"
+screen -dmS ellaparity parity --no-ui --author="0x0000000000" --unlock="0x0000000000" --password="~/community-pool/.walletpass" --max-peers=500 --extra-data="ella.miner" --identity="ella.miner" --cache=128 --chain "~/community-pool/ellaism.json"
 
 sleep 5
 
 #pool2b:
-screen -dmS ellapool2b ~/community-wallet/ellapool ~/community-wallet/pool2b.json
+screen -dmS ellapool2b ~/community-pool/ellapool ~/community-pool/pool2b.json
 
 #api:
-screen -dmS ellaapi ~/community-wallet/ellapool ~/community-wallet/api.json
+screen -dmS ellaapi ~/community-pool/ellapool ~/community-pool/api.json
 
 #unlocker:
-screen -dmS ellaunlocker ~/community-wallet/ellapool ~/community-wallet/unlocker.json
+screen -dmS ellaunlocker ~/community-pool/ellapool ~/community-pool/unlocker.json
 
 #payout:
-screen -dmS ellapayout ~/community-wallet/ellapool ~/community-wallet/payout.json
+screen -dmS ellapayout ~/community-pool/ellapool ~/community-pool/payout.json
 
 #netintelligence: 
 #cd ~/net-intelligence-api/ && pm2 start ~/net-intelligence-api/app.json
